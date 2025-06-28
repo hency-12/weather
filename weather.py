b6a274557377d645ee9d90c9bd634d014c14f213
hency-12 <hencyinternship@gmail.com>
2025-06-28 22:44:03 +0530

    weather.py

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model=pickle.load(open('F:/MachineLearning/Weather/weather_model (1).sav','rb'))
+loaded_model=pickle.load(open('weather_model (1).sav','rb'))
 
 def weather_prediction(input_data):
     
